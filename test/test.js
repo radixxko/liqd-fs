445519a0e70677371b60caeb4c8387196c38801a
Tomas Korenko <radixxko@gmail.com>
2019-01-08 12:45:10 +0100

    Version 1.1.0

--- test/test.js
+++ test/test.js
@@ -1,17 +1,115 @@
 'use strict';
 
+const ROOT = __dirname + '/fs_test/';
+
 const fs = require('fs');
 const FS = require('../lib/fs');
 
-try{ fs.mkdirSync( __dirname + '/fs_test' ); }catch(e){}
+const FilterDummyFiles = ( files ) => files.filter( file => !file.includes('/.'));
 
-FS.find( __dirname + '/fs_test', null, ( event, file ) =>
+const Sleep = ( ms ) => new Promise( resolve => setTimeout( resolve, ms ));
+
+const Exec = ( command ) => new Promise( resolve =>
 {
-	console.log( event, file );
-})
-.then( files =>
-{
-	console.log( files );
+	require('child_process').exec( command, ( err, stdout, stderr ) => resolve() );
 });
 
-setTimeout( process.exit, 100000 );
+const Save = ( filename, data ) => new Promise( async( resolve, reject ) =>
+{
+	await FS.mkdir( filename.replace( /\/[^\/]*$/, '' ));
+
+	fs.writeFile( filename, data, err => err ? reject( err ) : resolve() );
+});
+
+describe( 'FS', async( done ) =>
+{
+	it('should init tests', async function()
+	{
+		await Exec( 'rm -rf ' + ROOT + '*' );
+	});
+
+	it('should not find any files', async function()
+	{
+		let files = await FS.find( ROOT, null );
+
+		//console.log( FilterDummyFiles( files ));
+	});
+
+	it('should create new files and folders and emit changes', async function()
+	{
+		let files = await FS.find( ROOT, null, ( event, file ) =>
+		{
+			//console.log( event, file );
+		});
+
+		const new_files = new Set([ 'test.txt', 'a/a.test.txt', 'a/a/a.a.test.txt' ]);
+
+		for( let file of new_files )
+		{
+			await Save( ROOT + file, 'A' );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Exec( 'rm -f ' + ROOT + file );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Save( ROOT + file, 'A' );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Exec( 'rm -f ' + ROOT + file );
+		}
+
+		await Sleep( 1000 );
+	})
+	.timeout( 5000 );
+
+	it('should create new files and folders and emit changes with filter', async function()
+	{
+		let files = await FS.find( ROOT, /\.txt$/, ( event, file ) =>
+		{
+			//console.log( event, file );
+		});
+
+		const new_files = new Set([ 'test.txt', 'a/a.test.txt', 'a/a/a.a.test.txt' ]);
+
+		for( let file of new_files )
+		{
+			await Save( ROOT + file, 'A' );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Exec( 'rm -f ' + ROOT + file );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Save( ROOT + file, 'A' );
+		}
+
+		await Sleep( 1000 );
+
+		for( let file of new_files )
+		{
+			await Exec( 'rm -f ' + ROOT + file );
+		}
+
+		await Sleep( 1000 );
+	})
+	.timeout( 5000 );
+});
